d1d38010954588ff1016058b70e94aaf47530bd2
Moises <grimgust@gmail.com>
2018-09-29 14:05:20 -0600

    Enviar email con archivo

--- herramientas/MandarEmail.java
+++ herramientas/MandarEmail.java
@@ -10,7 +10,9 @@ import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 public class MandarEmail {
     private MandarEmail() {
     }
@@ -19,7 +21,7 @@ public class MandarEmail {
      * Enviar email por smtp
      *
      */
-    public static void Send(String recipiente, String titulo, String mensaje) throws AddressException, MessagingException {
+    public static void Send(String recipiente, String titulo, String mensaje, String nombreArchivo) throws AddressException, MessagingException {
         Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
 
@@ -37,7 +39,7 @@ public class MandarEmail {
         Session session = Session.getInstance(props, null);
 
         final MimeMessage msg = new MimeMessage(session);
-
+        DataSource source = new FileDataSource(nombreArchivo);
         // -- Set the FROM and TO fields --
         msg.setFrom(new InternetAddress("correoprogra00@gmail.com"));
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipiente, false));
@@ -46,7 +48,8 @@ public class MandarEmail {
         msg.setSubject(titulo);
         msg.setText(mensaje, "utf-8");
         msg.setSentDate(new Date());
-
+        msg.setDataHandler(new DataHandler(source));
+        msg.setFileName(nombreArchivo);
         SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
 
         t.connect("smtp.gmail.com", "correoprogra00", "55544412");
